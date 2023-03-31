351a82e60bef7103969e44176836af7ace92b9c1
Ralph Nicholson <nicholson.re@gmail.com>
2023-03-31 13:02:17 -0700

    Add button, add JS event listener

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+addingEventListener = () => {
+    const input = document.getElementById('button');
+    const clickAlert = () => {
+        alert("Hee Hee, that tickles!!!!");
+    };
+    input.addEventListener('click', clickAlert);
+};
+
+addingEventListener();
